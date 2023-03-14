5cb9e5d9c3dfc219e5796806226c527faf928685
JoshDT900 <DJTroutman900@gmail.com>
2023-03-14 16:50:39 -0400

    Add some assets and a form

---
+++ src/assets.js
@@ -0,0 +1,10 @@
+import gitImgLogo from "../src/imgs/GitHub-Mark-120px-plus.png"
+
+
+const assetMod = (function () {
+  const gitImg = gitImgLogo;
+
+  return { gitImg }
+})();
+
+export default assetMod;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import APIKEY from "../key/key.json"
 import css from "../src/style.css"
+import assetMod from "./assets";
 import { domEleGen } from "dom_gen_cosbert";
+import { doc } from "prettier";
 
 
-function navBar(body) {
+function navBarGen(body) {
   const bodyEle = body;
  
   const navWrap = domEleGen.makeEle("nav", "", ["class", "nav_wrap"])
@@ -19,7 +21,7 @@ function navBar(body) {
   externLinks.appendChild(createdEle);
 
   const gitImgArr = [
-    ["src", "../src/imgs/GitHub-Mark-120px-plus.png"],
+    ["src", assetMod.gitImg],
     ["alt", "Github Cat Logo"],
     ["class", "git_logo"],
   ]
@@ -32,10 +34,46 @@ function navBar(body) {
   externLinks.appendChild(gitLink).appendChild(gitImg)
 }
 
+function formGen(body) {
+  const mainEle = body;
+
+  const formEle = domEleGen.makeEle("form", "", ["action", ""]);
+  mainEle.appendChild(formEle);
+
+  const zipLabel = domEleGen.makeEle("label", "Enter a ZIPCODE. (USA Only)", ["for", "zipcode"]);
+  formEle.appendChild(zipLabel);
+
+  const zipInputArr = [
+    ["type", "text"],
+    ["name", "zipcode"],
+    ["id", "zipcode"],
+    ["pattern", "[0-9]{5}"],
+  ]
+  const zipInput = domEleGen.makeEle("input", "", zipInputArr)
+  formEle.appendChild(zipInput);
+
+  const formSubBtn = domEleGen.makeEle("button", "Submit", ["type", "submit"]);
+  formEle.addEventListener("submit", (e) => {
+    e.preventDefault();
+  })
+  formEle.appendChild(formSubBtn);
+
+}
+
+function mainGen(body) {
+  const bodyEle = body;
+
+  const mainEleWrap = domEleGen.makeEle("div", "", ["class", "main_wrap"]);
+  bodyEle.appendChild(mainEleWrap);
+
+  formGen(mainEleWrap);
+}
+
 function pageRender() {
   const bodyEle = document.querySelector("body");
 
-  navBar(bodyEle)
+  navBarGen(bodyEle)
+  mainGen(bodyEle);
 }
 
 (() => {
