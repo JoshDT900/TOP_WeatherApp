12738eb8d2bd901c855d2814bb15766b2758ee53
JoshDT900 <DJTroutman900@gmail.com>
2023-03-14 14:49:55 -0400

    Add assets and style sheet

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 // import APIKEY from "../key/key.json"
 
 
+function navBar(body) {
+  const bodyEle = body;
+
+  console.log(bodyEle);
+  
+
+}
+
+function pageRender() {
+  const bodyEle = document.querySelector("body");
+
+  navBar(bodyEle)
+}
+
+(() => {
+  pageRender();
+})();
